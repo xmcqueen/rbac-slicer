88394827706d4e72dc2a06621144d41d83ccabb1
Brian McQueen <bmcqueen@linkedin.com>
2023-04-16 16:53:08 -0700

    added sort by value

--- main.go
+++ main.go
@@ -74,15 +74,38 @@ func main() {
 	resourcesCounter := countThem(resources)
 	verbsCounter := countThem(verbs)
 
-	for _, k := range sortKeys(resourcesCounter) {
-		fmt.Println(k, resourcesCounter[k])
+	for _, p := range rankByWordCount(resourcesCounter) {
+		fmt.Println(p.Key, p.Value)
 	}
 
-	for _, k := range sortKeys(verbsCounter) {
-		fmt.Println(k, verbsCounter[k])
+	for _, p := range rankByWordCount(verbsCounter) {
+		fmt.Println(p.Key, p.Value)
 	}
 
 	return
+}
+
+//https://stackoverflow.com/questions/18695346/how-can-i-sort-a-mapstringint-by-its-values
+type Pair struct {
+  Key string
+  Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int){ p[i], p[j] = p[j], p[i] }
+
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+  pl := make(PairList, len(wordFrequencies))
+  i := 0
+  for k, v := range wordFrequencies {
+    pl[i] = Pair{k, v}
+    i++
+  }
+  sort.Sort(sort.Reverse(pl))
+  return pl
 }
 
 func sortKeys(dat map[string]int) (rv []string) {
